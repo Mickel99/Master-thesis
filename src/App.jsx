1ca402d0c8ffacec8ed0594497a1cc121cb9a4af
Mickel99 <mickel.mounirji@gmail.com>
2024-01-18 14:21:41 +0100

    Light mode and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import Header1 from "./components/header/header1"
-import Header2 from "./components/header/header2"
-import Header3 from "./components/header/header3"
-
+import Header1 from "./components/header/header1";
+import Header2 from "./components/header/header2";
+import Header3 from "./components/header/header3";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
+  const [theme, colorMode] = useMode();
 
   return (
-    <>
-    <Header1 />
-    <Header2 />
-    <Header3 />
-
-    </>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header1 />
+        <Header2 />
+        <Header3 />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
