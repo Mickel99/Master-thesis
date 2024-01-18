9feccf1526b07b198922f83b7fde6fc9af8ac4b8
Mickel99 <mickel.mounirji@gmail.com>
2024-01-18 15:39:47 +0100

    header 1 styling

--- src/components/header/header1.jsx
+++ src/components/header/header1.jsx
@@ -1,43 +1,115 @@
-import   { useContext } from "react";
+import { useContext } from "react";
 import { ColorModeContext } from "../../theme";
-import { IconButton, useTheme } from "@mui/material";
+import {
+  Box,
+  Container,
+  IconButton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const Header1 = () => {
-    const colorMode = useContext(ColorModeContext);
-    const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+  const theme = useTheme();
 
-    return (
+  return (
+    <Box
+      sx={{
+        bgcolor: "#A9957B",
+        padding: "8px",
+        borderBottomRightRadius: 20,
+        borderBottomLeftRadius: 20,
+
+      }}
+    >
+      <Container>
+      <Stack direction={"row"} alignItems={"center"}>
+        <Typography
+          sx={{
+            mr: 2,
+            p: "4px 10px",
+            bgcolor: "#D23F57",
+            borderRadius: "12px",
+            fontSize: "12px",
+            fontWeight: "bold",
+            color: "#fff",
+          }}
+          variant="body2"
+        >
+          Sale
+        </Typography>
+
+        <Typography
+          sx={{
+            fontSize: "12px",
+            fontWeight: 300,
+            color: "#fff",
+            
+          }}
+          variant="body2"
+        >
+          Free Express Shipping
+        </Typography>
+
+        <Box flexGrow={1} />
         <div>
-        {theme.palette.mode === "light" ? (
-          <IconButton
-            onClick={() => {
-              localStorage.setItem(
-                "mode",
-                theme.palette.mode === "dark" ? "light" : "dark"
-              );
-              colorMode.toggleColorMode();
-            }}
-            color="inherit"
-          >
-            <LightModeOutlined />
-          </IconButton>
-        ) : (
-          <IconButton
-            onClick={() => {
-              localStorage.setItem(
-                "mode",
-                theme.palette.mode === "dark" ? "light" : "dark"
-              );
-              colorMode.toggleColorMode();
-            }}
-            color="inherit"
-          >
-            <DarkModeOutlined />
-          </IconButton>
-        )}
-      </div>
-    );
-}
+          {theme.palette.mode === "light" ? (
+            <IconButton
+              onClick={() => {
+                localStorage.setItem(
+                  "mode",
+                  theme.palette.mode === "dark" ? "light" : "dark"
+                );
+                colorMode.toggleColorMode();
+              }}
+              color="inherit"
+            >
+              <LightModeOutlined fontSize="small" />
+            </IconButton>
+          ) : (
+            <IconButton
+              onClick={() => {
+                localStorage.setItem(
+                  "mode",
+                  theme.palette.mode === "dark" ? "light" : "dark"
+                );
+                colorMode.toggleColorMode();
+              }}
+              color="inherit"
+            >
+              <DarkModeOutlined fontSize="small" />
+            </IconButton>
+          )}
+        </div>
+
+        <TwitterIcon
+          sx={{
+            fontSize: "16px",
+            color: "#fff",
+          }}
+        />
+        <FacebookIcon
+          sx={{
+            fontSize: "16px",
+            mx: 1,
+            color: "#fff",
+          }}
+        />
+        <InstagramIcon
+          sx={{
+            fontSize: "16px",
+            color: "#fff",
+          }}
+        />
+      </Stack>
+      </Container>
+    </Box>
+  );
+};
 
 export default Header1;
