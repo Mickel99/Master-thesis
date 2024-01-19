28ba8d39d157eab1eb7bc075b545ba6df43e2f67
Mickel99 <mickel.mounirji@gmail.com>
2024-01-19 11:22:54 +0100

    Created header 2

--- src/components/header/header2.jsx
+++ src/components/header/header2.jsx
@@ -1,11 +1,105 @@
-import React from 'react';
-
+import PersonIcon from '@mui/icons-material/Person';
+import {
+  IconButton,
+  Container,
+  Stack,
+  Typography,
+} from "@mui/material";
+import {
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
+ 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
+ 
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+ 
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+ 
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+ 
 const Header2 = () => {
-    return (
-        <div>
-            header2
-        </div>
-    );
-}
 
+ 
+  return (
+    <Container sx={{ my: 3, display: "flex", justifyContent: "space-between" }}>
+      <Stack>
+        {/* logo change later */}
+        <ShoppingCartOutlined />
+        <Typography variant="body2">ExamensBok</Typography>
+      </Stack>
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+        />
+      </Search>
+      
+
+      
+      <Stack direction={"row"} alignItems={"center"}>
+
+      <IconButton aria-label="cart">
+          <StyledBadge badgeContent={4} color="primary">
+            <ShoppingCartIcon />
+          </StyledBadge>
+        </IconButton>
+        
+        <IconButton>
+          <PersonIcon />
+        </IconButton>
+ 
+
+      </Stack>
+    </Container>
+  );
+};
 export default Header2;
